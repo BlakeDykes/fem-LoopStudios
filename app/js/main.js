390aeea60ee9a8f74acea572f1a2749cc76c48d8
Blake Dykes <blakedykes0104@gmail.com>
2021-02-03 18:21:39 -0600

    Finished footer and added hover animations

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,9 @@
 const burgerIcon = document.querySelector('#burgerIcon');
 const nav = document.querySelector('.nav');
 const body = document.querySelector('body');
+const creations = document.querySelectorAll('.filter__black');
 
+//Nav
 burgerIcon.addEventListener('click', function(){
     //Close Nav
     if(nav.classList.contains('open')){
@@ -17,6 +19,19 @@ burgerIcon.addEventListener('click', function(){
         body.classList.add('no-scroll');
         console.log("burger got clicked - open");
     }
+});
 
-    
+//Hover Creations change text to black
+creations.forEach(element => {
+    element.addEventListener("mouseover", function(){
+        const searchText = "t".concat(element.id);
+        document.getElementById(searchText).style.color='black';
+    });
+});
+
+//Mouse Exit for Creations change text to white
+creations.forEach(element => {
+    element.addEventListener("mouseleave", function(){
+        document.getElementById("t".concat(element.id)).style.color='white';
+    });
 });
